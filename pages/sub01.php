8ebee489dd6e9dac126d0e97eedc783b586ba3f5
ssw0428 <ssw0428@naver.com>
2021-12-28 13:58:57 +0900

    rebuild

--- pages/sub01.php
+++ pages/sub01.php
@@ -3,7 +3,7 @@ include_once ($_SERVER['DOCUMENT_ROOT']."/resources/include/head.php");
 ?>
 
 <div class="sub_wrap exam_form">
-    <div class="sub_header" style="background-image:url()"></div>
+    <div class="sub_header" style="background-image:url('https://ssw0428.github.io./vision/resources/images/main-slide-1.jpg')">sss</div>
 </div>
 
 <?php
